dc98c6405c840cf2f1c9054389aceaf12d15f138
Damian Ruczynski <damian.m.ruczynski@gmail.com>
2023-12-27 16:45:44 +0100

    display window add icon

---
+++ headers/config.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+constexpr short SCREEN_HEIGHT = 255;
+constexpr short SCREEN_WIDTH = 255;
+constexpr int SCREEN_RESIZE = 2;
+constexpr std::chrono::microseconds FRAME_DURATION(16667);
+
+
+//hrefs
+
+ std::string iconRef = "C:\\Users\\damia\\Desktop\\MAIN\\PJATK\\thirdSemester\\PJC\\platformGame\\assets\\icon.jpg";
--- main.cpp
+++ main.cpp
@@ -1,21 +1,65 @@
-#include <iostream>
-#include <SFML/Graphics.hpp>
 #include <fmt/core.h>
+#include <chrono>
+#include <cmath>
+#include <SFML/Graphics.hpp>
+#include "headers/config.hpp"
+
 
 int main() {
-    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML window");
+    //starting configuration
+    char current_level = 0;
+    short level_finish = 0;
+    std::chrono::microseconds lag(0);
+    std::chrono::steady_clock::time_point previous_time;
+    //SFML window configuration
+    sf::Color background_color = sf::Color(225, 112, 85);
+    sf::Event event;
+    sf::RenderWindow window(sf::VideoMode(SCREEN_RESIZE * SCREEN_WIDTH, SCREEN_RESIZE * SCREEN_HEIGHT),
+                            "Catch Catchy", sf::Style::Close);
+    window.setPosition({static_cast<int>(window.getPosition().x), static_cast<int>(window.getPosition().y - 90)});
 
-    while (window.isOpen())
-    {
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
-        window.clear();
-        window.display();
+    sf::View view(sf::FloatRect(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT));
+    previous_time = std::chrono::steady_clock::now();
+
+    //icon load
+    auto image = sf::Image{};
+    if (!image.loadFromFile(iconRef)){
+        fmt::println("Cannot load icon");
+    }else {
+        window.setIcon(image.getSize().x, image.getSize().y, image.getPixelsPtr());
     }
 
+    // open window
+    while (window.isOpen()) {
+        auto delta_time = std::chrono::duration_cast<std::chrono::microseconds>(
+                std::chrono::steady_clock::now() - previous_time);
+        lag += delta_time;
+        previous_time += delta_time;
+        while (FRAME_DURATION <= lag) {
+            int view_x;
+            lag -= FRAME_DURATION;
+            while (window.pollEvent(event)) {
+                switch (event.type) {
+                    case sf::Event::Closed:
+                        window.close();
+                        break;
+                    case sf::Event::KeyPressed:
+                        switch (event.key.code) {
+                            case sf::Keyboard::Enter:
+                                fmt::println("Enter pressed");
+                                break;
+                        }
+                        break;
+                }
+            }
+
+            if (FRAME_DURATION > lag) {
+                view.reset(sf::FloatRect(view_x, 0, SCREEN_WIDTH, SCREEN_HEIGHT));
+                window.setView(view);
+                window.clear(background_color);
+                window.display();
+            }
+        }
+    }
     return EXIT_SUCCESS;
 }
